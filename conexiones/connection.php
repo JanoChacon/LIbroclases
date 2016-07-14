3b32be5b8f7d1fd8f01c6cb210b8a6ef165ed485
JanoChacon <jano.xhacon@gmail.com>
2016-07-14 19:22:25 -0400

    Update connection.php
    cambio clave

--- conexiones/connection.php
+++ conexiones/connection.php
@@ -1,7 +1,7 @@
 <?php
 //conexion a la base de datos, (discrecion con la passs)
 	function conectar(){
-		$db = new mysqli('localhost','root','pac-man2','SchoolSys');
+		$db = new mysqli('localhost','root','laclave','SchoolSys');
 		if($db->connect_errno > 0){
 		    die('Imposible conectar ['.$db->connect_error.']');
 		}
